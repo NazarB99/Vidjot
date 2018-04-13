de1a0fb2f6e01c817396336d31644d3adf0ec4bf
nazar99bayramsahatov@gmail.com <nazar99bayramsahatov@gmail.com>
2018-04-13 18:02:51 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/vidjot-dev')
+.then(() => console.log('DB connected'))
+.catch(err => console.log(err));
+
+require('./models/Idea');
+const Idea = mongoose.model('ideas')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.render('index')
+});
+
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+
+app.get('/ideas/add',(req,res) =>{
+    res.render('ideas/add');
+})
+
+app.post('/ideas',(req,res) => {
+    let errors = [];
+
+    if(!req.body.title){
+        errors.push({text:'Please fill out title input'});
+    }
+    if(!req.body.details){
+        errors.push({text:'Please fill out details input'});
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add',{
+            errors,
+            title:req.body.title,
+            detail:req.body.details
+        });
+    }
+    else{
+        res.send('passed')
+    }
+
+});
+
+const port = 5000;
+
+app.listen(port,() => {
+    console.log(`Server started on port ${port}`)
+});
